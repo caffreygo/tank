7f9cd0748a054d3b27c9baad48f5b43cf871a3a3
Jerry Chen <caffreygo@163.com>
2022-03-08 23:16:25 +0800

    减小向下调转方向的概率

--- src/model/tank.ts
+++ src/model/tank.ts
@@ -14,7 +14,7 @@ export default class extends modelAbstract implements IModel {
 
   render(): void {
     this.move();
-    if (_.random(20) === 1) {
+    if (_.random(50) === 1) {
       this.direction = directionEnum.bottom;
     }
   }
